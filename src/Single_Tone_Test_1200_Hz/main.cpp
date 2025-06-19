3b695bd68af62fffe08739e1569ca1f0d1339b64
Сергей Новиков <s.novikov@vedapro.ru>
2025-06-19 18:41:33 +0300

    проверить как отправляется строка

--- src/Single_Tone_Test_1200_Hz/main.cpp
+++ src/Single_Tone_Test_1200_Hz/main.cpp
@@ -5,11 +5,38 @@
 // #include "rssi_sbar.hpp"
 // #include <string.h>
 #include "Single_Tone_Test_1200_Hz.hpp"
+#include <stdint.h>
+
+#define UART_BASE 0x4006B800
+
+const char *a = "test";
+
+typedef volatile struct {
+   uint32_t DR;    // Data Register
+   uint32_t SR;    // Status Register
+   uint32_t BRR;   // Baud Rate Register
+   uint32_t CR1;   // Control Register 1
+   uint32_t CR2;   // Control Register 2
+} UART_TypeDef;
+
+static UART_TypeDef *const uart = (UART_TypeDef *)UART_BASE;
+
+void uart_send_char(char c) {
+    while (!(uart->SR & (1 << 7))) {}  // Ждём флаг TXE (Transmit Empty)
+    uart->DR = c;
+}
+
+void uart_send_str(const char *s) {
+    while (*s) {
+        uart_send_char(*s++);
+    }
+}
 
 CSingle_Tone_Test_1200_Hz Single_Tone_Test_1200_Hz;
 
 int main()
 {
+   uart_send_str(a);
    IRQ_RESET();
    return 0;
 }
