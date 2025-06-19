a13f6e4f83e30e9bc6a371c33a84221f4929ba76
Сергей Новиков <s.novikov@vedapro.ru>
2025-06-19 15:59:23 +0300

    добавил свои исходники

---
+++ src/Single_Tone_Test_1200_Hz/main.cpp
@@ -0,0 +1,35 @@
+#include "system.hpp"
+#include "hardware/hardware.hpp"
+#include "registers.hpp"
+// #include "uv_k5_display.hpp"
+// #include "rssi_sbar.hpp"
+// #include <string.h>
+
+int main()
+{
+    
+    return 0;
+}
+
+extern "C" void Reset_Handler()
+{
+   IRQ_RESET();
+}
+
+extern "C" void SysTick_Handler()
+{
+   static bool bFirstInit = false;
+   if(!bFirstInit)
+   {
+      System::CopyDataSection();
+      __libc_init_array();
+      bFirstInit = true;
+   }
+   
+   static unsigned int u32StupidCounter = 1;
+   if((!(u32StupidCounter++ % 2) && u32StupidCounter > 200))
+   {
+    //   Pong.Handle();
+   }
+    IRQ_SYSTICK();
+}
